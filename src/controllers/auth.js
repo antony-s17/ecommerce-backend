613630bd4b6ac61e3a78e7b2f88914c75acfc0f0
Antony Salas <salasra1795@gmail.com>
2026-09-12 14:39:28 -0500

    fix: update hhtp code fro login and logout

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,6 +1,5 @@
 import { insertUser, loginUser, selectUser } from "../services/auth.js";
 import CError, { Selector } from "../misc/errors.js";
-import { isValidUUID } from '../utils/utils.js';
 
 const createUser = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -33,29 +32,48 @@ const createUserAdmin = async (req, res, next) => {
 }
 
 const login = async (req, res, next) => {
-    const { email, password } = req.body;
-    const response = await loginUser(email, password);
-    if (!response.ok) {
-        return next(new CError(Selector.BAD_ERROR));
+  const { email, password } = req.body;
+
+  const response = await loginUser( email, password);
+
+  if (!response.ok) {
+    if (
+      response.data === Selector.WRONG_CRED
+    ) {
+      return next(
+        new CError(
+          Selector.WRONG_CRED
+        )
+      );
     }
-    res.cookie("access_token", response.data, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
-        maxAge: 60 * 60 * 1000
-    });
-    return res.status(200).json(
-        {
-            ok: true
-        }
-    )
-}
+    return next(
+      new CError(
+        Selector.BAD_ERROR
+      )
+    );
+  }
+
+  const isProduction = process.env.NODE_ENV === "production";
+
+  res.cookie("access_token", response.data, {
+    httpOnly: true,
+    secure: isProduction,
+    sameSite: isProduction ? "none" : "lax",
+    maxAge: 60 * 60 * 1000
+  });
+
+  return res.status(200).json({
+    ok: true
+  });
+};
 
 const logout = async(req, res, next) => {
+    const isProduction = process.env.NODE_ENV === "production";
+
     res.clearCookie("access_token", {
-        httpOnly: true,
-        secure: true,
-        sameSite: "none"
+    httpOnly: true,
+    secure: isProduction,
+    sameSite: isProduction ? "none" : "lax"
     });
     return res.status(200).json(
         {
