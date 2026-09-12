10440bfbc343fd472b04927bebe70ab8f3b4f9a9
Antony Salas <salasra1795@gmail.com>
2026-09-12 14:38:25 -0500

    fix: update hhtp code fro login and logout

--- src/services/auth.js
+++ src/services/auth.js
@@ -22,35 +22,63 @@ const insertUser = async (user, role) => {
     }
 }
 
+import { compare } from "bcrypt";
+import { prisma } from "../db/config.js";
+import { sign } from "../utils/jwt.js";
+import { Selector } from "../misc/errors.js";
 
-const loginUser = async (email, password ) => {
-    try {
-        const user = await prisma.user.findUnique(
-        {
-            select: {id: true, password: true, role: true },
-            where: { email }
-        });
-        if (!user) {
-            throw new Error("Doesn't create user");
-        }
-        const areEqual = await compare(password, user.password);
-        if (!areEqual) {
-            throw new Error("Invalid credentials");
-        }
-        const { role } = user;
+const loginUser = async (email, password) => {
+  try {
+    const user = await prisma.user.findUnique({
+      select: {
+        id: true,
+        password: true,
+        role: true,
+      },
+      where: {
+        email,
+      },
+    });
 
-        const token = sign({ id: user.id, email, role });
-
-        return {
-            ok: true,
-            data: token
-        }
-    } catch(error) {
-        return {
-            ok: false
-        }
+    if (!user) {
+      return {
+        ok: false,
+        data: Selector.WRONG_CRED,
+      };
     }
-}
+
+    const areEqual = await compare(
+      password,
+      user.password
+    );
+
+    if (!areEqual) {
+      return {
+        ok: false,
+        data: Selector.WRONG_CRED,
+      };
+    }
+
+    const token = sign({
+      id: user.id,
+      email,
+      role: user.role,
+    });
+
+    return {
+      ok: true,
+      data: token,
+    };
+
+  } catch (error) {
+    console.error("LOGIN SERVICE ERROR:", error);
+
+    return {
+      ok: false,
+      data: Selector.BAD_ERROR,
+    };
+  }
+};
 
 const selectUser = async (id) => {
     try {
